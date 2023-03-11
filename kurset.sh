2daa1ab279254adf8ef54ba1ab41509e983dceb2
Abraham Akerele <abrahamakerele38@gmail.com>
2023-03-11 21:08:36 +0100

    add option to make master schedulable

--- kurset.sh
+++ kurset.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+kurser_help() {
+  echo "
+Command: kurset
+Usage:
+  kurset  --help # Show this help information
+  kurset  worker # Build worker nodes
+  kurset         # build worker node
+  kurset --schedule # build master node and also make it a worker node
+  "
+  exit 1
+}
+
+
 if [[ $1 = "worker" ]];
 then
     ./scripts/worker.sh
 else
-    ./scripts/master.sh 
+    ./scripts/master.sh
+    case "$1" in
+        --schedule|-s)
+            kubectl taint nodes --all node-role.kubernetes.io/master-
+            kubectl taint nodes --all  node-role.kubernetes.io/control-plane-
+            ;;
+        *)
+        kurser_help
+        ;;
+    esac
 fi
