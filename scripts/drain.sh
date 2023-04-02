30b93c219ba3503af74db12ed046c6cb9c58522b
akerele abraham <abrahamakerele38@gmail.com>
2023-04-02 17:19:04 +0100

    set --grace-period=0

--- scripts/drain.sh
+++ scripts/drain.sh
@@ -4,7 +4,7 @@ delete_nodes () {
    for n in $(kubectl get nodes | awk '{if (NR!=1) {print $1}}');
    do
       echo "Deleting node $n"
-      kubectl drain $n --ignore-daemonsets --delete-local-data || true 
+      kubectl drain $n --ignore-daemonsets --delete-local-data --force --grace-period=0  || true 
       kubectl delete node $n || true
    done
 }
--- scripts/master.sh
+++ scripts/master.sh
@@ -22,8 +22,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo ufw allow 2379 && sudo ufw allow 2380 && sudo ufw allow 6443 && sudo ufw allow 8090 && sudo ufw allow  8091 && sudo ufw allow 8472 && sudo ufw allow 10250 && sudo ufw allow 10251 && sudo ufw allow  10252 && sudo ufw allow  10255 && sudo ufw allow 179 && sudo ufw allow 4789 && sudo ufw allow 4 && sudo ufw allow  5473 && sudo ufw allow  51820 && sudo ufw allow  51821 && sudo ufw allow 4789
 
 
-network = $network
-
 if [[ $network = "flannel" ]];
 then
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
